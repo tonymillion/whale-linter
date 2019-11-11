bbba3031318b01e3e773e7980e46a45d42a828f2
Tony Million <tonymillion@gmail.com>
2019-11-10 17:29:23 -0800

    Fixed FROM <image> AS <name>
    Again black formatter took a go at the files :/

--- whalelinter/token.py
+++ whalelinter/token.py
@@ -20,7 +20,7 @@ class Token:
         self.pointless_commands = App._pointless_commands
 
 
-@Dispatcher.register(token='add')
+@Dispatcher.register(token="add")
 class Add(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
@@ -32,19 +32,21 @@ class Add(Token):
         return True
 
     def download_from_url(self):
-        if ('http://' in self.payload[0] or 'https://' in self.payload[0]):
+        if "http://" in self.payload[0] or "https://" in self.payload[0]:
             App._collecter.throw(3004, line=self.line)
             return True
         return False
 
 
-@Dispatcher.register(token='copy')
+@Dispatcher.register(token="copy")
 class Copy(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
 
         # Do not check path if the copy uses a previous stage
-        if not any(arg.startswith('--from=') for arg in self.payload) and not App._dockerfile.get('is_remote'):
+        if not any(
+            arg.startswith("--from=") for arg in self.payload
+        ) and not App._dockerfile.get("is_remote"):
             self.check_path()
 
     def check_path(self):
@@ -55,27 +57,27 @@ class Copy(Token):
             full_path = file_to_copy
 
             if not os.path.isabs(file_to_copy):
-                directory = os.path.dirname(os.path.abspath(App._args.get('DOCKERFILE')))
-                full_path = directory + '/' + file_to_copy
+                directory = os.path.dirname(
+                    os.path.abspath(App._args.get("DOCKERFILE"))
+                )
+                full_path = directory + "/" + file_to_copy
 
             if not os.path.exists(full_path):
                 App._collecter.throw(
                     1004,
                     line=self.line,
-                    keys={
-                        'file': file_to_copy,
-                        'directory': directory
-                    })
+                    keys={"file": file_to_copy, "directory": directory},
+                )
 
 
-@Dispatcher.register(token='expose')
+@Dispatcher.register(token="expose")
 class Expose(Token):
     def __init__(self, ports, line):
         Token.__init__(self, __class__, ports, line)
 
         for port in ports:
-            if '-' in port:
-                ports = ports + port.split('-')
+            if "-" in port:
+                ports = ports + port.split("-")
                 ports.remove(port)
 
         for port in ports:
@@ -83,29 +85,28 @@ class Expose(Token):
             self.is_tcp_or_udp(port)
 
     def is_in_range(self, port):
-        if '/' in port:
-            port = port.split('/')[0]
+        if "/" in port:
+            port = port.split("/")[0]
 
         if int(port) < 1 or int(port) > 65535:
-            App._collecter.throw(2005, line=self.line, keys={'port': port})
+            App._collecter.throw(2005, line=self.line, keys={"port": port})
             return False
         return True
 
     def is_tcp_or_udp(self, port):
-        if '/' in port:
-            if port.split('/')[1] != 'tcp' and port.split('/')[1] != 'udp':
+        if "/" in port:
+            if port.split("/")[1] != "tcp" and port.split("/")[1] != "udp":
                 App._collecter.throw(2009, line=self.line)
                 return False
         return True
 
 
-@Dispatcher.register(token='label')
+@Dispatcher.register(token="label")
 class Label(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
-        self.payload = Tools.merge_odd_strings(
-            Tools.sanitize_list(self.payload))
-        self.labels = {l.split('=')[0]: l.split('=')[1] for l in self.payload}
+        self.payload = Tools.merge_odd_strings(Tools.sanitize_list(self.payload))
+        self.labels = {l.split("=")[0]: l.split("=")[1] for l in self.payload}
 
         self.is_namespaced()
         self.uses_reserved_namespaces()
@@ -113,39 +114,31 @@ class Label(Token):
 
     def is_namespaced(self):
         for key in self.labels.keys():
-            if key.count('.') < 2:
-                App._collecter.throw(3005, line=self.line, keys={'label': key})
+            if key.count(".") < 2:
+                App._collecter.throw(3005, line=self.line, keys={"label": key})
 
     def uses_reserved_namespaces(self):
         for key in self.labels.keys():
-            for reserved_namespaces in [
-                    'com.docker', 'io.docker', 'org.dockerproject'
-            ]:
+            for reserved_namespaces in ["com.docker", "io.docker", "org.dockerproject"]:
                 if key.startswith(reserved_namespaces):
                     App._collecter.throw(
-                        2014,
-                        line=self.line,
-                        keys={
-                            'label': reserved_namespaces
-                        })
+                        2014, line=self.line, keys={"label": reserved_namespaces}
+                    )
 
     def uses_valid_characters(self):
         for key in self.labels.keys():
-            if not re.match('^[a-z0-9-.]+$', key):
-                App._collecter.throw(1003, line=self.line, keys={'label': key})
+            if not re.match("^[a-z0-9-.]+$", key):
+                App._collecter.throw(1003, line=self.line, keys={"label": key})
 
 
-@Dispatcher.register(token='maintainer')
+@Dispatcher.register(token="maintainer")
 class Maintainer(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
-        App._collecter.throw(
-            2013, line=self.line, keys={
-                'instruction': 'MAINTAINER'
-            })
+        App._collecter.throw(2013, line=self.line, keys={"instruction": "MAINTAINER"})
 
 
-@Dispatcher.register(token='run')
+@Dispatcher.register(token="run")
 class Run(Token):
     def __init__(self, payload, line):
         payload = list(filter(None, payload))
@@ -157,18 +150,19 @@ class Run(Token):
         shell_arguments = self.payload[1:]
         next_command_index = False
 
-        if '&&' in shell_arguments:
-            next_command_index = shell_arguments.index('&&')
-            next_command = shell_arguments[(next_command_index + 1):]
+        if "&&" in shell_arguments:
+            next_command_index = shell_arguments.index("&&")
+            next_command = shell_arguments[(next_command_index + 1) :]
             shell_arguments = shell_arguments[:next_command_index]
 
-        if shell_command in Dispatcher._callbacks['RUN']:
-            if Dispatcher._callbacks['RUN'][shell_command]['self'] is not None:
-                Dispatcher._callbacks['RUN'][shell_command]['self'](
-                    token='RUN',
+        if shell_command in Dispatcher._callbacks["RUN"]:
+            if Dispatcher._callbacks["RUN"][shell_command]["self"] is not None:
+                Dispatcher._callbacks["RUN"][shell_command]["self"](
+                    token="RUN",
                     command=shell_command,
                     args=shell_arguments,
-                    lineno=line)
+                    lineno=line,
+                )
 
         if next_command_index and next_command:
             self.__init__(next_command, self.line)
@@ -178,14 +172,13 @@ class Run(Token):
     def is_pointless(self):
         if self.payload[0] in self.pointless_commands:
             App._collecter.throw(
-                2003, line=self.line, keys={
-                    'command': self.payload[0]
-                })
+                2003, line=self.line, keys={"command": self.payload[0]}
+            )
             return True
         return False
 
 
-@Dispatcher.register(token='from')
+@Dispatcher.register(token="from")
 class SourceImage(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
@@ -194,49 +187,55 @@ class SourceImage(Token):
         self.has_latest_tag()
 
     def is_too_long(self):
-        if len(self.payload) > 1:
-            App._collecter.throw(
-                1002, line=self.line, keys={
-                    'command': self.payload
-                })
+        # payload could be 1 item FROM image:tag
+        # payload could be 3 items FROM image:tag AS thing
+
+        param_count = len(self.payload)
+
+        if param_count not in [1, 3]:
+            App._collecter.throw(1002, line=self.line, keys={"command": self.payload})
             return True
+
+        # additional checks
+        if param_count == 3:
+            if self.payload[1].upper() != "AS":
+                App._collecter.throw(1005, line=self.line, keys={"command": "FROM",
+                                                                 "specifier": "AS"})
+                return True
         return False
 
     def has_no_tag(self):
-        if ':' not in self.payload[0]:
-            App._collecter.throw(
-                2000, line=self.line, keys={
-                    'image': self.payload[0]
-                })
+        if ":" not in self.payload[0]:
+            App._collecter.throw(2000, line=self.line, keys={"image": self.payload[0]})
             return True
         return False
 
     def has_latest_tag(self):
-        if ':latest' in self.payload[0]:
+        if ":latest" in self.payload[0]:
             App._collecter.throw(2001, line=self.line)
             return True
         return False
 
 
-@Dispatcher.register(token='user')
+@Dispatcher.register(token="user")
 class User(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
 
     def is_becoming_root(self):
-        if self.payload[0] == 'root':
+        if self.payload[0] == "root":
             App._collecter.throw(2007, line=self.line)
             return True
         return False
 
 
-@Dispatcher.register(token='workdir')
+@Dispatcher.register(token="workdir")
 class Workdir(Token):
     def __init__(self, payload, line):
         Token.__init__(self, __class__, payload, line)
 
     def has_relative_path(self):
-        if not payload[0].startswith('/'):
+        if not payload[0].startswith("/"):
             App._collecter.throw(2004, line=self.line)
             return True
         return False
--- whalelinter/utils.py
+++ whalelinter/utils.py
@@ -6,39 +6,38 @@ from whalelinter.app import App
 
 
 COLORS = {
-    'BLUE'  : '\033[94m',
-    'GREEN' : '\033[92m',
-    'YELLOW': '\033[93m',
-    'RED'   : '\033[91m',
-    'ENDC'  : '\033[0m',
-    'BOLD'  : '\033[1m',
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "YELLOW": "\033[93m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+    "BOLD": "\033[1m",
 }
 
 
 class Tools:
-
     @staticmethod
     def sanitize_list(lst):
-        lst = [i for i in lst if i != '']
+        lst = [i for i in lst if i != ""]
         return lst
 
     @staticmethod
     def merge_odd_strings(lst):
         bad_splitted_string = False
         for s in lst:
-            if '=' not in s:
+            if "=" not in s:
                 bad_splitted_string = True
 
         if bad_splitted_string:
             indexes = []
             for string in lst:
-                if '=' not in string:
+                if "=" not in string:
                     if not string.startswith('"'):
                         indexes.append(lst.index(string))
 
             for idx in reversed(indexes):
-                lst[idx - 1] = str(lst[idx - 1]) + ' ' + str(lst[idx])
-                lst[idx] = ''
+                lst[idx - 1] = str(lst[idx - 1]) + " " + str(lst[idx])
+                lst[idx] = ""
 
         return Tools.sanitize_list(lst)
 
@@ -51,14 +50,14 @@ class DockerfileCommand:
         self._raw = None
 
     def __repr__(self):
-        return self.instruction + ' ' + ' '.join(self.arguments)
+        return self.instruction + " " + " ".join(self.arguments)
 
 
 class Log:
     def __init__(self, rule, line, message):
-        self.rule     = rule
-        self.category = rule.get('category')
-        self.message  = message
+        self.rule = rule
+        self.category = rule.get("category")
+        self.message = message
 
         if line is None:
             line = 0
@@ -66,55 +65,73 @@ class Log:
         self.line = line
 
     def display(self, color, category_right_padding, line_number_right_padding):
-        category_right_padding = category_right_padding * ' '
-        line_number_right_padding = line_number_right_padding * ' '
+        category_right_padding = category_right_padding * " "
+        line_number_right_padding = line_number_right_padding * " "
 
         if self.line:
-            print('  {}{}: {}{}{} : {}{}'.format(line_number_right_padding, self.line, COLORS.get(color.upper()), self.category, category_right_padding, COLORS.get('ENDC'), self.message))
+            print(
+                "  {}{}: {}{}{} : {}{}".format(
+                    line_number_right_padding,
+                    self.line,
+                    COLORS.get(color.upper()),
+                    self.category,
+                    category_right_padding,
+                    COLORS.get("ENDC"),
+                    self.message,
+                )
+            )
         else:
-            print('      {}{}{} : {}{}'.format(COLORS.get(color.upper()), self.category, category_right_padding, COLORS.get('ENDC'), self.message))
+            print(
+                "      {}{}{} : {}{}".format(
+                    COLORS.get(color.upper()),
+                    self.category,
+                    category_right_padding,
+                    COLORS.get("ENDC"),
+                    self.message,
+                )
+            )
 
     def displayLight(self, level):
-        print('{}:{}'.format(level, self.message))
+        print("{}:{}".format(level, self.message))
 
 
 class Collecter:
     def __init__(self, rules, ignore=None):
-        self.ignore      = ignore
-        self.rules       = rules
-        self.log_classes = App._config['log_classes']
+        self.ignore = ignore
+        self.rules = rules
+        self.log_classes = App._config["log_classes"]
 
     def get_level_by_category(self, category):
         for log_class, v in self.log_classes.items():
-            if category in v.get('categories'):
+            if category in v.get("categories"):
                 return log_class
         return None
 
     def get_rule_by_id(self, rule_id):
         for rule in self.rules:
-            if rule.get('id') == str(rule_id):
+            if rule.get("id") == str(rule_id):
                 return rule
         return None
 
     def throw(self, id, **kwargs):
         if str(id) not in self.ignore:
-            rule    = self.get_rule_by_id(id)
-            message = rule.get('message')
+            rule = self.get_rule_by_id(id)
+            message = rule.get("message")
 
-            if kwargs.get('keys'):
-                message = message.format(**(kwargs.get('keys')))
+            if kwargs.get("keys"):
+                message = message.format(**(kwargs.get("keys")))
 
-            log   = Log(rule, kwargs.get('line'), message)
+            log = Log(rule, kwargs.get("line"), message)
             level = self.get_level_by_category(log.category)
 
-            if 'logs' not in self.log_classes[level]:
-                self.log_classes[level]['logs'] = []
+            if "logs" not in self.log_classes[level]:
+                self.log_classes[level]["logs"] = []
 
-            self.log_classes[level]['logs'].append(log)
+            self.log_classes[level]["logs"].append(log)
 
     def find_longest_category_name(self, level):
         length = 0
-        for category in self.log_classes[level].get('categories'):
+        for category in self.log_classes[level].get("categories"):
             if len(category) > length:
                 length = len(category)
 
@@ -123,63 +140,80 @@ class Collecter:
     def find_highest_line_number(self):
         length = 0
         for log_class, v in self.log_classes.items():
-            if 'logs' in v:
-                for log in v.get('logs'):
+            if "logs" in v:
+                for log in v.get("logs"):
                     if len(str(log.line)) > length:
                         length = len(str(log.line))
 
         return length
 
     def display(self):
-        logs = {
-            'critical': False,
-            'warning':  False,
-            'enhancement': False
-        }
+        logs = {"critical": False, "warning": False, "enhancement": False}
 
         for log_class, v in self.log_classes.items():
-            if 'logs' in v:
+            if "logs" in v:
                 logs[log_class] = True
 
-        if logs['critical'] or logs['warning'] or logs['enhancement']:
+        if logs["critical"] or logs["warning"] or logs["enhancement"]:
 
-            if App._args.get('json'):
-                output = {log_class: {c: {} for c in v.get('categories')} for log_class, v in self.log_classes.items()}
+            if App._args.get("json"):
+                output = {
+                    log_class: {c: {} for c in v.get("categories")}
+                    for log_class, v in self.log_classes.items()
+                }
 
                 for log_class, v in self.log_classes.items():
                     if logs.get(log_class):
-                        for log in v.get('logs'):
-                            output[log_class][v.get('category')] = log.__dict__
+                        for log in v.get("logs"):
+                            output[log_class][v.get("category")] = log.__dict__
 
                 print(json.dumps(output))
 
-            elif App._args.get('no_color'):
+            elif App._args.get("no_color"):
                 for log_class, v in self.log_classes.items():
-                    categories = v.get('categories')
+                    categories = v.get("categories")
                     if logs.get(log_class):
-                        for log in v.get('logs'):
+                        for log in v.get("logs"):
                             if log.category in categories:
                                 log.displayLight(log_class)
             else:
                 for log_class, v in self.log_classes.items():
-                    color      = v.get('color').upper()
-                    categories = v.get('categories')
+                    color = v.get("color").upper()
+                    categories = v.get("categories")
 
                     if logs.get(log_class):
-                        print('{}{} :{}'.format(COLORS.get(color), log_class.upper(), COLORS.get('ENDC')))
-                        for log in v.get('logs'):
+                        print(
+                            "{}{} :{}".format(
+                                COLORS.get(color), log_class.upper(), COLORS.get("ENDC")
+                            )
+                        )
+                        for log in v.get("logs"):
                             if log.category in categories:
-                                category_right_padding = self.find_longest_category_name(log_class) - len(log.category)
-                                line_number_right_padding = self.find_highest_line_number() - len(str(log.line))
-                                log.display(color, category_right_padding, line_number_right_padding)
+                                category_right_padding = self.find_longest_category_name(
+                                    log_class
+                                ) - len(
+                                    log.category
+                                )
+                                line_number_right_padding = self.find_highest_line_number() - len(
+                                    str(log.line)
+                                )
+                                log.display(
+                                    color,
+                                    category_right_padding,
+                                    line_number_right_padding,
+                                )
                         print()
 
-            if logs.get('critical'):
+            if logs.get("critical"):
                 exit(3)
-            elif logs.get('warning'):
+            elif logs.get("warning"):
                 exit(2)
-            elif logs.get('enhancement'):
+            elif logs.get("enhancement"):
                 exit(1)
         else:
-            print('{}{}{}\n'.format(COLORS.get('GREEN'), 'Everything is good', COLORS.get('ENDC')))
+            print(
+                "{}{}{}\n".format(
+                    COLORS.get("GREEN"), "Everything is good", COLORS.get("ENDC")
+                )
+            )
             exit(0)
